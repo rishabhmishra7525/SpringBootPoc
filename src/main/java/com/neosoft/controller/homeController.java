817ef42dcc6ed5b2d38a3ec97e3763e6b5db2bfa
rishabhmishra7525 <rishabhmishra7525@gmail.com>
2021-12-23 17:58:29 +0530

    Ui Chnageg

--- src/main/java/com/neosoft/controller/homeController.java
+++ src/main/java/com/neosoft/controller/homeController.java
@@ -3,16 +3,45 @@ package com.neosoft.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.neosoft.util.Common;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class homeController {
 
 	@GetMapping("/")
 	public String showPage(Model model) {
-
 		model.addAttribute("labelName", Common.getService());
+		model.addAttribute("skill", Common.skillLave());
+		
+		
 		return "homePage.html";
 	}
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String data() {
+		
+		return "1/5";
+	}
+	
+	
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public @ResponseBody String processAJAXRequest(
+	            @RequestParam("communication") String firstname,
+	            @RequestParam("basicweb") String lastname   ) {
+	        String response = firstname+lastname;
+	        // Process the request
+	        // Prepare the response string
+	        return response;
+	    }
+	
+	
 }
--- src/main/java/com/neosoft/util/Common.java
+++ src/main/java/com/neosoft/util/Common.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Common {
 
 	public static List<String> getService() {
-		return List.of("", " ");
+		return List.of("PHP-0-2", "PHP-2-4","PHP-5+");
+	}	
+	
+	public static List<String> skillLave() {
+		return List.of("lavel1", "lavel2","lavel3","lavel4","lavel5");
 	}	
 	
 }
